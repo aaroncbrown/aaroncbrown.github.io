dc7f87600ba3d57a055d8d913962dbdf8cb51337
Aaron Brown <aaroncbrown1993@gmail.com>
2022-02-10 23:45:52 -0500

    implemented snap scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import Contact from './components/Contact/Contact';
+import Intro from './components/Intro/Intro';
+import Portfolio from './components/Portfolio/Portfolio';
+import Testimonials from './components/Testimonials/Testimonials';
 import TopBar from './components/TopBar/TopBar';
+import Works from './components/Works/Works';
+import './app.scss';
 
 function App() {
   return (
     <div className="app">
       <TopBar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
     </div>
   );
 }
