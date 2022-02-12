8cef0264c903afc5e1cc38b8db3280f14245dd72
Aaron Brown <aaroncbrown1993@gmail.com>
2022-02-12 11:49:32 -0500

    Updated intro section

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -2,6 +2,22 @@ import './intro.scss';
 
 export default function Intro(): JSX.Element {
   return (
-    <div id='intro' className='intro'></div>
+    <div id='intro' className='intro'>
+      <div className='left'>
+        <div className="imgContainer">
+          <img src='images/angels-landing-summit.jpg' alt="Angel's Landing Summit" />
+        </div>
+      </div>
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hi there, I'm</h2>
+          <h1>Aaron Brown</h1>
+          <h3>Software Engineer</h3>
+        </div>
+        <a href='#portfolio'>
+          <img src='images/down-arrow.png' alt='down arrow' />
+        </a>
+      </div>
+    </div>
   )
 }
