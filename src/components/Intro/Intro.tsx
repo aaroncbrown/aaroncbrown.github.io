7aa3fd543c82853fc94ca8dd1f8c25eddccf5d84
Aaron Brown <aaroncbrown1993@gmail.com>
2022-02-13 16:07:32 -0500

    took away width 100% on portrait mode

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -14,11 +14,11 @@ function getIntroBackground(aspectRatio: number): JSX.Element {
   // Add 2 to numerator of 1920 by 1080 to allow for strange decimal behavior.
   if (aspectRatio <= (1922 / 1080)) {
     // Standard monitors.
-    return <img src='images/angels-landing-summit-4032x1977.jpg' alt="Angel's Landing Summit Portrait" className='intro-picture' />;
+    return <img src='images/angels-landing-summit-4032x1977.jpg' alt="Angel's Landing Summit Portrait" className='intro-picture fit-picture' />;
   }
 
   // Wider than 16 by 9 aspect ratio (i.e. ultrawide monitors).
-  return <img src='images/angels-landing-summit-4032x1079.jpg' alt="Angel's Landing Summit Portrait" className='intro-picture' />;
+  return <img src='images/angels-landing-summit-4032x1079.jpg' alt="Angel's Landing Summit Portrait" className='intro-picture fit-picture' />;
 }
 
 export default function Intro(): JSX.Element {
