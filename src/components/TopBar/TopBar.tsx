5a51b3060bf770c8f587b1cf44a96d627269d544
Aaron Brown <aaroncbrown1993@gmail.com>
2022-02-13 15:18:40 -0500

    Added linkedin link

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,5 +1,6 @@
 import './top-bar.scss'
 import MailIcon from '@mui/icons-material/Mail';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { Dispatch, SetStateAction } from 'react';
 
 interface TopBarParams {
@@ -15,10 +16,15 @@ export default function TopBar({ menuOpen, setMenuOpen }: TopBarParams): JSX.Ele
           <a href='#intro' className='logo'>
             <img src='images/logo512.png' alt="Aaron's site logo." className='logo-image' />
           </a>
-          <div className="itemContainer">
+          <div className='itemContainer'>
             <MailIcon className='icon' />
             <span>aaroncbrown@gmail.com</span>
           </div>
+          <div className='itemContainer'>
+            <LinkedInIcon className='icon' />
+            <a target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/aaron-brown-305036a1/'>LinkedIn Profile</a>
+          </div>
+          
         </div>
         <div className='right'>
           <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
