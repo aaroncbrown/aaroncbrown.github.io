439c3f3948cab69f738ac554bbe965489e21cef8
Aaron Brown <aaroncbrown1993@gmail.com>
2022-02-13 15:51:37 -0500

    Updated style for logo and links if aspect ration too small

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -9,21 +9,25 @@ interface TopBarParams {
 }
 
 export default function TopBar({ menuOpen, setMenuOpen }: TopBarParams): JSX.Element {
+  const aspectRatio = window.screen.width / window.screen.height;
+
   return (
     <div className={'top-bar' + (menuOpen ? ' active' : '')}>
       <div className='wrapper'>
         <div className='left'>
           <a href='#intro' className='logo'>
-            <img src='images/logo512.png' alt="Aaron's site logo." className='logo-image' />
+            <img src='images/logo512.png' alt="Aaron's site logo." />
           </a>
-          <div className='itemContainer'>
-            <MailIcon className='icon' />
-            <span>aaroncbrown@gmail.com</span>
-          </div>
-          <div className='itemContainer'>
-            <LinkedInIcon className='icon' />
-            <a target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/aaron-brown-305036a1/'>LinkedIn Profile</a>
-          </div>    
+          <div className={'item-container-wrapper' + (aspectRatio < .6 ? ' display-none' : '')}>
+            <div className='item-container'>
+              <MailIcon className='icon' />
+              <span>aaroncbrown@gmail.com</span>
+            </div>
+            <div className='item-container'>
+              <LinkedInIcon className='icon' />
+              <a target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/aaron-brown-305036a1/'>LinkedIn Profile</a>
+            </div>   
+          </div> 
         </div>
         <div className='right'>
           <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
