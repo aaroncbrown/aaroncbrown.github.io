a4b3fb36dc57ba7207187885e741842ba85fe18a
Aaron Brown <aaroncbrown1993@gmail.com>
2022-02-16 22:51:29 -0500

    Updated portfolio section to have skylight virtual agent

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -2,6 +2,41 @@ import './portfolio.scss';
 
 export default function Portfolio(): JSX.Element {
   return (
-    <div id='portfolio' className='portfolio'></div>
+    <div id='portfolio' className='portfolio'>
+      <div className='section-title'>Portfolio</div>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className='container'>
+        <div className='item'>
+          <img src='images/skylight-virtual-agent.png' alt='Skylight Virtual Agent' />
+          <div className='item-title'>Skylight Virtual Agent</div>
+        </div>
+        <div className='item'>
+          <img src='images/skylight-virtual-agent.png' alt='Skylight Virtual Agent' />
+          <div className='item-title'>Skylight Virtual Agent</div>
+        </div>
+        <div className='item'>
+          <img src='images/skylight-virtual-agent.png' alt='Skylight Virtual Agent' />
+          <div className='item-title'>Skylight Virtual Agent</div>
+        </div>
+        <div className='item'>
+          <img src='images/skylight-virtual-agent.png' alt='Skylight Virtual Agent' />
+          <div className='item-title'>Skylight Virtual Agent</div>
+        </div>
+        <div className='item'>
+          <img src='images/skylight-virtual-agent.png' alt='Skylight Virtual Agent' />
+          <div className='item-title'>Skylight Virtual Agent</div>
+        </div>
+        <div className='item'>
+          <img src='images/skylight-virtual-agent.png' alt='Skylight Virtual Agent' />
+          <div className='item-title'>Skylight Virtual Agent</div>
+        </div>
+      </div>
+    </div>
   )
 }
