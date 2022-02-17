a16c0330e38670449cf171e59f35f8535ec67b6d
Aaron Brown <aaroncbrown1993@gmail.com>
2022-02-16 22:15:12 -0500

    reverted aspect ration calculation

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -22,7 +22,7 @@ function getIntroBackground(aspectRatio: number): JSX.Element {
 }
 
 export default function Intro(): JSX.Element {
-  const aspectRatio = window.innerWidth / window.innerHeight;
+  const aspectRatio = window.screen.width / window.screen.height;
 
   return (
     <div id='intro' className='intro'>
